bc0185bcc2629e6dff3afe4e61f88ab5215763fb
Ben Herring <ben_herring@hotmail.com>
2018-09-14 00:48:25 +0100

    added a lot of stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(address: "2 priest end, thame")
