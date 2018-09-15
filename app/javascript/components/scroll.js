aa3fd0a917715cfb1f8315d4b515d39004b32828
Ben Herring <ben_herring@hotmail.com>
2018-09-15 15:49:15 +0100

    changes

---
+++ app/javascript/components/scroll.js
@@ -0,0 +1,27 @@
+
+function scroll(){
+$(document).ready(function(){
+
+  $("a").on('click', function(event) {
+
+
+    if (this.hash !== "") {
+
+      event.preventDefault();
+
+
+      var hash = this.hash;
+
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+}
+export { scroll };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
 import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText();
+
+import { scroll } from '../components/scroll';
+scroll();
