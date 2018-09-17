7df7f2f15e35e7f4363d28e5e42cb34547a49058
Ben Herring <ben_herring@hotmail.com>
2018-09-17 20:44:44 +0100

    added projects section

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
 
    def index
-    @users = User.where.not(latitude: nil, longitude: nil)
+    @projects = Project.all
+    # render 'projects/index'
+     # render 'projects/index'
+    # @users = User.where.not(latitude: nil, longitude: nil)
 
-    @markers = @users.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude#,
-        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
-      }
+    # @markers = @users.map do |user|
+    #   {
+    #     lat: user.latitude,
+    #     lng: user.longitude,
+    #     icon: "images/orangepin.png"
+    #     # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+    #   }
     end
   end
-end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(address: "2 priest end, thame")
+
+Project.create(name: "Gimme Shelter", description: "A mobile website created a Le Wagon in a group. It enables someone to book a hostel bed for a homeless person that night and show nearby services.", photo: "gimme-shelter.png")
